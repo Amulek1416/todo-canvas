a94c2c4081bab8ce99c64f63f3b4410482fd9090
Amulek1416 <zcarico03@gmail.com>
2019-05-10 23:31:14 +0000

    Created class to handle settings and file locations

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    def __init__(self):
+        self.calendarPath = ""
+
+    def getCalendarPath(self):
+        return self.calendarPath
+
+    def setCalendarPath(self, path):
+        self.calendarPath = path
