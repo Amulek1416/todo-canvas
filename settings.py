217d9b70bbde0d64c0883fc64178ae50711b5329
Amulek1416 <zcarico03@gmail.com>
2019-05-11 14:16:49 +0000

    Made settings file able to load from json file

--- settings.py
+++ settings.py
@@ -1,9 +1,26 @@
+import json
+from collections import namedtuple
+
+settingsPath = 'resources/settings.json'
+
 class Settings:
-    def __init__(self):
-        self.calendarPath = ""
+    def __init__(self, data):
+        self.__dict__ = json.load(data)
 
     def getCalendarPath(self):
-        return self.calendarPath
+        return self.icsPath
 
     def setCalendarPath(self, path):
-        self.calendarPath = path
+        self.icsPath = path
+        # self.saveSettings()
+
+    def saveSettings(self):
+        with open('resources/settings.json', 'w') as outfile:
+            json.dump(self, outfile)
+
+def loadSettings():
+    settingsFile = open(settingsPath)
+    setting = Settings(settingsFile)
+    settingsFile.close()
+    return setting
+
