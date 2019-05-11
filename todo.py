925b1caa226c280d586028e7537c890e403e08c4
Amulek1416 <zcarico03@gmail.com>
2019-05-10 21:58:27 -0600

    started work for parsing .ics files

---
+++ todo.py
@@ -0,0 +1,20 @@
+import datetime
+import icalendar
+
+if __name__ == '__main__':
+   main()
+
+def main():
+
+   return
+
+class Todo:
+   def __init__(self):
+      return
+
+
+
+class Assignment:
+   def __init__(self):
+      self.name = ""
+      self.dueDate = ""
