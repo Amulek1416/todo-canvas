53bff370f4f89736eb03f49510e9d3e494d681b4
Amulek1416 <zcarico03@gmail.com>
2019-05-10 21:57:37 -0600

    created class to handle API calls for canvas

---
+++ canvasAPI.py
@@ -0,0 +1,9 @@
+class canvasAPI:
+   def __init__(self):
+      self.token = ""
+      self.school = ""
+      self.urlTODO = "https://" + self.school + ".instructure.com/api/v1/users/self/todo"
+
+   def getTODO(self):
+      return
+   
